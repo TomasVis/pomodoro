fe551e5542dace19b1bcdb96f0728217141538d2
TomasVis <visokiams@gmail.com>
2019-09-22 22:29:12 +0300

    initial user stories

--- src/App.js
+++ src/App.js
@@ -2,25 +2,84 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+/*
+
+
+User Story #11: When I click the element with the id of reset, any running timer should be stopped, the value within id="break-length"should return to 5, the value within id="session-length"should return to 25, and the element with id="time-left"should reset to it's default state.
+User Story #12: When I click the element with the id of break-decrement, the value within id="break-length"decrements by a value of 1, and I can see the updated value.
+User Story #13: When I click the element with the id of break-increment, the value within id="break-length"increments by a value of 1, and I can see the updated value.
+User Story #14: When I click the element with the id of session-decrement, the value within id="session-length"decrements by a value of 1, and I can see the updated value.
+User Story #15: When I click the element with the id of session-increment, the value within id="session-length"increments by a value of 1, and I can see the updated value.
+User Story #16: I should not be able to set a session or break length to <= 0.
+User Story #17: I should not be able to set a session or break length to > 60.
+User Story #18: When I first click the element with id="start_stop", the timer should begin running from the value currently displayed in id="session-length", even if the value has been incremented or decremented from the original value of 25.
+User Story #19: If the timer is running, the element with the id of time-leftshould display the remaining time in mm:ssformat (decrementing by a value of 1 and updating the display every 1000ms).
+User Story #20: If the timer is running and I click the element with id="start_stop", the countdown should pause.
+User Story #21: If the timer is paused and I click the element with id="start_stop", the countdown should resume running from the point at which it was paused.
+User Story #22: When a session countdown reaches zero (NOTE: timer MUST reach 00:00), and a new countdown begins, the element with the id of timer-labelshould display a string indicating a break has begun.
+User Story #23: When a session countdown reaches zero (NOTE: timer MUST reach 00:00), a new break countdown should begin, counting down from the value currently displayed in the id="break-length"element.
+User Story #24: When a break countdown reaches zero (NOTE: timer MUST reach 00:00), and a new countdown begins, the element with the id of timer-labelshould display a string indicating a session has begun.
+User Story #25: When a break countdown reaches zero (NOTE: timer MUST reach 00:00), a new session countdown should begin, counting down from the value currently displayed in the id="session-length"element.
+User Story #26: When a countdown reaches zero (NOTE: timer MUST reach 00:00), a sound indicating that time is up should play. This should utilize an HTML5 audiotag and have a corresponding id="beep".
+User Story #27: The audio element with id="beep"must be 1 second or longer.
+User Story #28: The audio element with id of beepmust stop playing and be rewound to the beginning when the element with the id of resetis clicked.*/
+class App extends React.Component {
+  constructor(props){
+    super(props)
+this.state ={
+
+}
+this.start = Date.now();
+this.startTimer = this.startTimer.bind(this)
+
+  }
+ 
+
+startTimer = () => {
+  this.setState({
+    timerOn: true,
+    timerTime: this.state.timerTime,
+    timerStart: Date.now() - this.state.timerTime
+  });
+  this.timer = setInterval(() => {
+    this.setState({
+      timerTime: Date.now() - this.state.timerStart
+    });
+  }, 10);
+};
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <body>
+
+        <div>
+          <h2 id="break-label">Break Length</h2><h2 id="session-label">Session Length</h2>
+          <button id="break-decrement" >V</button>
+          <button id="session-decrement" >V</button>
+          <button id="break-increment" >^</button>
+          <button id="session-increment" >^</button>
+
+          <div id="break-length">5</div>
+          <div id="session-length">25</div>
+
+          <h2 id="timer-label">Session</h2>
+          <h2 id="time-left" ></h2>
+
+          <button id="start_stop" >Start/stop</button>
+          <button id="reset" >Reset</button>
+
+        </div>
+
+
+      </body>
     </div>
   );
+}
 }
 
 export default App;
